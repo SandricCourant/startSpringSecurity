44aa163581a61cfb9e1a9abc18ee6aff29bb0a6a
Sandric <36101178+SandricCourant@users.noreply.github.com>
2023-02-14 19:10:40 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controllers;
+
+import com.example.demo.controllers.dto.UserDto;
+import com.example.demo.services.UserService;
+import com.example.demo.services.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<UserDto>> getAll(){
+
+        Iterable<User> users = userService.getAll();
+
+        Collection<UserDto> usersDto = new ArrayList<>();
+        users.forEach(user -> usersDto.add(user.toUserDto()));
+
+        return ResponseEntity.status(200).body(usersDto);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> get(@PathVariable int id){
+
+        User user = userService.get(id);
+        if(user == null) return ResponseEntity.status(409).body(null);
+        UserDto userDto = user.toUserDto();
+
+        return ResponseEntity.status(200).body(userDto);
+    }
+    @PostMapping("/")
+    public ResponseEntity<UserDto> create(@RequestBody UserDto requestDto){
+        User user = userService.create(requestDto.toUser());
+        if(user == null) return ResponseEntity.status(409).body(null);
+        UserDto result = user.toUserDto();
+        return ResponseEntity.status(201).body(result);
+    }
+    @PutMapping("/{id}/validateEmail")
+    public boolean validateEmail(@PathVariable int id){
+        return userService.validateEmail(id);
+    }
+    @PutMapping("/{id}/validatePhone/{code}")
+    public boolean validatePhone(@PathVariable int id, @PathVariable int code){
+        return userService.validatePhone(id, code);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable int id){
+        return userService.delete(id);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,56 @@
+package com.example.demo.services;
+
+import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Iterable<User> getAll(){
+        return userRepository.findAll();
+    }
+    public User get(int id){
+        return userRepository.findById(id).orElse(null);
+    }
+    public User create(User user){
+
+        try {
+            return userRepository.save(user);
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+    public boolean validateEmail(int id){
+
+        try{
+            User user = userRepository.findById(id).orElse(null);
+            user.setEmailValidated(true);
+            userRepository.save(user);
+            return true;
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+    public boolean delete(int id) {
+        try{
+            User user = userRepository.findById(id).orElse(null);
+            userRepository.delete(user);
+            return true;
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean validatePhone(int id, int code) {
+        return true;
+    }
+}
